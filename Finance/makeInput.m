9530ba9b6f7773c33ef76c7cd930493d296db946
davidgore <david@debianG3.net>
2025-10-03 10:19:05 -0400

    Commit from Debian

--- Finance/makeInput.m
+++ Finance/makeInput.m
@@ -18,12 +18,12 @@ for i=1:n
   f.SetFolder('etf');
   f.SetFile(strcat(date,'-fidelity-',strcat(symbol,'-d.csv')));
   f.SetSymbol(symbol);
-  f.Save(strcat(symbol,'-d'));
+  f.Save(strcat(symbol,'-d')); % TODO
 
   clear f;
   f=FidelityFile;
   f.SetFolder('etf');
   f.SetFile(strcat(date,'-fidelity-',strcat(symbol,'-w.csv')));
   f.SetSymbol(symbol);
-  f.Save(strcat(symbol,'-w'));
+  f.Save(strcat(symbol,'-w')); % TODO
 endfor
