c5f33631e3b0875941c3f6d70a7b20529e307e17
dgore03 <davidgore@protonmail.com>
2026-03-09 11:09:57 -0400

    Added DoWhatToBuy.m for WhatToBuy script

--- Test/DoPlot.m
+++ Test/DoPlot.m
@@ -1,8 +1,6 @@
 function [] = DoPlot(ticker)
 
-##  baseFolder = fileparts(fileparts(fileparts(pwd()))); % for debugging in Octave
-
-  baseFolder = fileparts(fileparts(pwd())); % assumes calling from Batch folder
+  baseFolder = fileparts(fileparts(fileparts(fileparts(mfilename('fullpath')))));
   projectsFolder = fullfile(baseFolder,'Projects');
   octaveFolder = fullfile(projectsFolder,'Octave');
 
