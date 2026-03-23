4a77aa43a2fdb73367013efc22146bf2784ee119
dgore03 <davidgore@protonmail.com>
2026-03-23 14:18:30 -0400

    Added Median, Midrange and Slope to DoPlot.m

--- Test/DoPlot.m
+++ Test/DoPlot.m
@@ -12,5 +12,14 @@ function [] = DoPlot(ticker)
   z.LoadFile(filename);
   z.Plot;
 
+  t = z.GetTimestamp;
+  x = z.GetValue;
+
+  n = min(10,length(x)); % consider last 10 minutes
+  [vals,coeffs] = Util.GetSignal(t(end-n+1:end),x(end-n+1:end));
+
+  fprintf('Median price = %.2f, Midrange price = %.2f, Slope of signal (last 10 values) = %.2f\n',...
+    median(x), (min(x)+max(x))/2, coeffs(1));
+
 endfunction
 
