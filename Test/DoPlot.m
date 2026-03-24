da14d271559e27b07098201620550341be8f615b
dgore03 <davidgore@protonmail.com>
2026-03-24 15:31:48 -0400

    Added velocity, acceleration to Plot.m

--- Test/DoPlot.m
+++ Test/DoPlot.m
@@ -12,14 +12,25 @@ function [] = DoPlot(ticker)
   z.LoadFile(filename);
   z.Plot;
 
+
   t = z.GetTimestamp;
   x = z.GetValue;
 
-  n = min(10,length(x)); % consider last 10 minutes
+  [signal,macd,~,~] = z.MACD.CalcMACD(x);
+  vel = macd;
+  acc = macd-signal;
+
+  n = min(5,length(x)); % consider last 5 minutes
   [vals,coeffs] = Util.GetSignal(t(end-n+1:end),x(end-n+1:end));
 
-  fprintf('Median price = %.2f, Midrange price = %.2f, Slope of signal (last 10 values) = %.2f\n',...
-    median(x), (min(x)+max(x))/2, coeffs(1));
+  fprintf('Median price = %.2f, Midrange price = %.2f\n',median(x), (min(x)+max(x))/2);
+  fprintf('Slope of signal (last 5 values) = %.2f\n',coeffs(1));
+  fprintf('Velocity (last 5):\t');
+  fprintf('%10.2f ',vel(end-n+1:end));
+  fprintf('\n');
+  fprintf('Acceleration (last 5):\t');
+  fprintf('%10.2f ',acc(end-n+1:end));
+  fprintf('\n');
 
 endfunction
 
