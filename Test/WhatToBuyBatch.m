b580ca9e7ba004d333c05775be79f865578ea822
dgore03 <davidgore@protonmail.com>
2026-05-07 11:43:28 -0400

    Added IQM(-) and Sell Limit to WhatToBuyBatch output

--- Test/WhatToBuyBatch.m
+++ Test/WhatToBuyBatch.m
@@ -25,7 +25,7 @@ fid_write = fopen(fullfile(batchFolder,'WhatToBuyBatchResult.csv'),'w');
 if fid_write == -1
   error("Failed to open output file for writing.");
 endif
-fprintf(fid_write,"File,Accel Test,EMA Test,Last Price,IQM(+),Max Bid,Buy Line Extrap,Range,Nr. Samples,ROR,Trend Slope,Velocity,Acceleration\n");
+fprintf(fid_write,"File,Accel Test,EMA Test,Last Price,IQM(+),Max Bid,IQM(-),Sell Limit,Buy Line Extrap,Range,Nr. Samples,ROR,Trend Slope,Velocity,Acceleration\n");
 DoWhatToBuyBatch(fid_write,f,tickernames,cfg);
 fclose(fid_write);
 
